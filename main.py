348e1b0a3ba5d2141c92751ba0297c8769955b18
shaaran Lakshminarayanan <34472894+devshaaran@users.noreply.github.com>
2018-10-13 02:31:08 +0530

    new changes
    updt

--- main.py
+++ main.py
@@ -4,26 +4,45 @@ from matplotlib.widgets import RectangleSelector
 import time
 from pydarknet import Detector, Image
 import cv2
+from operator import itemgetter
+from pyautogui import size
+import html_headers
+
 import numpy as np
 # global constants
 img = None
 tl_list = (2,2)
 br_list = (2,2)
+label = 0
+text = 1
+header = 2
+block_text = 3
+button = 4
+image = 5
+paragraph = 6
+textarea = 7
+x_res,y_res = size()
+
+
 
 # sys.path.insert(0, '/home/shaaran/PycharmProjects/om/YOLO3-4-Py')
-
-
+# x.sort(key=itemgetter(1))
+# this is what you use for it
 
 def yolo():
 
     if __name__ == "__main__":
         # Optional statement to configure preferred GPU. Available only in GPU version.
         # pydarknet.set_cuda_device(0)
+        # fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+
 
         net = Detector(bytes("cfg/yolov3.cfg", encoding="utf-8"), bytes("weights/yolov3.weights", encoding="utf-8"), 0,
                        bytes("cfg/coco.data", encoding="utf-8"))
 
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture('projectile.mp4')
+        # out = cv2.VideoWriter('output.avi', fourcc, 20.0, (848, 480))
+
         r,cropper = cap.read()
         targeter(cropper)
 
@@ -42,8 +61,13 @@ def yolo():
 
                 end_time = time.time()
                 print("Elapsed Time:", end_time - start_time, 'final_result',results)
+                total_list = []
+                reg_decider = []
+                x_val = []
+                item_space = []
 
                 for cat, score, bounds in results:
+
                     x, y, w, h = bounds
                     cv2.rectangle(frame, (int(x - w / 2), int(y - h / 2)), (int(x + w / 2), int(y + h / 2)),
                                   (255, 0, 0))
@@ -51,79 +75,140 @@ def yolo():
                                 (255, 255, 0))
 
 
+
+                    if cat.decode("utf-8") == 'label':
+                        main_param  = 0
+                    elif cat.decode("utf-8") == 'text':
+                        main_param  = 1
+                    elif cat.decode("utf-8") == 'header':
+                        main_param  = 2
+                    elif cat.decode("utf-8") == 'block_text':
+                        main_param  = 3
+                    elif cat.decode("utf-8") == 'button':
+                        main_param  = 4
+                    elif cat.decode("utf-8") == 'image':
+                        main_param  = 5
+                    elif cat.decode("utf-8") == 'paragraph':
+                        main_param  = 6
+                    else:
+                        main_param  = 7
+
+
+                    total_list.append([main_param,int(y - h / 2),int(y + h / 2)])
+                    item_space.append(int(y + h / 2)-int(y - h / 2))
+                    x_val.append([int(x - w / 2),int(x + w / 2)])
+
+                    left_x = int(x - w / 2)
+                    right_x = int(x + w / 2)
+
+                    reg0 = 0
+                    reg1 = (x_res / 2) - (x_res / 2.2)
+                    reg2 = (x_res / 2) - (x_res / 4)
+                    reg3 = (x_res / 2)
+                    reg4 = (x_res / 2) + (x_res / 4)
+                    reg5 = (x_res / 2) + (x_res / 2.2)
+                    reg6 = x_res
+
+                    if left_x < reg1 and right_x > reg5:
+                        reg_decider.append(0)
+
+                    elif left_x < reg2 and left_x > reg1 and right_x < reg3:
+                        reg_decider.append(1)
+
+
+                total_list.sort(key=itemgetter(1))
+                space_list = []
+                space_percent = []
+                computer_res_fixture = []
+
+                content_list = []
+
+
+                label_turner = False
+
+                for i in range(0,len(total_list)-1):
+                    if i == 0:
+
+                        space_list.append(total_list[i][1]-0)
+                        space_percent.append((space_list[0]/y)*100)
+                        computer_res_fixture.append(x_res-(x_res/8)*space_percent[0])
+                        space_list.append(total_list[i+1][1]-total_list[i][2])
+                        space_percent.append((space_list[i+1] / y) * 100)
+                        computer_res_fixture.append(x_res - (x_res / 8) * space_percent[i+1])
+
+                    else:
+
+                        space_list.append(total_list[i + 1][1] - total_list[i][2])
+                        space_percent.append((space_list[i + 1] / y) * 100)
+                        computer_res_fixture.append((x_res - (x_res / 8)) * (space_percent[i+1]/100))
+
+                        if label_turner == True:
+                            continue
+
+                        if total_list[i][0] == 0 or total_list[i][0] == 1:
+                            if space_percent[i+1] < 0 and total_list[i][0] != total_list[i+1][0] :
+                                content_list.append(html_headers.html_tools('style = margin-top: '+ computer_res_fixture[i]+ 'vh' )['input_text_label'])
+                                label_turner = True
+                                continue
+
+                        if total_list[i][0] == 0 or total_list[i][0] == 7:
+                            if space_percent[i + 1] < 0 and total_list[i][0] != total_list[i + 1][0]:
+                                content_list.append(html_headers.html_tools('style = margin-top: '+ computer_res_fixture[i]+ 'vh')['input_textarea_label'])
+                                label_turner = True
+                                continue
+
+                        if total_list[i][0] == 0 :
+                            content_list.append(html_headers.html_tools('style = margin-top: '+ computer_res_fixture[i]+ 'vh')['label'])
+
+                        elif total_list[i][0] == 1:
+                            content_list.append(
+                                html_headers.html_tools('style = margin-top: ' + computer_res_fixture[i] + 'vh')[
+                                    'input_text'])
+                        elif total_list[i][0] == 2:
+                            content_list.append(
+                                html_headers.html_tools('style = margin-top: ' + computer_res_fixture[i] + 'vh')[
+                                    'header'])
+                        elif total_list[i][0] == 3:
+                            content_list.append(
+                                html_headers.html_tools('style = margin-top: ' + computer_res_fixture[i] + 'vh')[
+                                    'block_text'])
+                        elif total_list[i][0] == 4:
+                            content_list.append(
+                                html_headers.html_tools('style = margin-top: ' + computer_res_fixture[i] + 'vh')[
+                                    'button'])
+                        elif total_list[i][0] == 5:
+                            content_list.append(
+                                html_headers.html_tools('style = margin-top: ' + computer_res_fixture[i] + 'vh')[
+                                    'image'])
+                        elif total_list[i][0] == 0:
+                            content_list.append(
+                                html_headers.html_tools('style = margin-top: ' + computer_res_fixture[i] + 'vh')[
+                                    'paragraph'])
+                        else:
+                            content_list.append(
+                                html_headers.html_tools('style = margin-top: ' + computer_res_fixture[i] + 'vh')[
+                                    'input_textarea'])
+
+                print(space_percent)
+
+                # out.write(frame)
                 cv2.imshow("preview", frame)
-                cv2.imshow('hey',frame_cropped)
-
-            k = cv2.waitKey(1)
-            if k == 0xFF & ord("q"):
-                break
+                # cv2.imshow('hey',frame_cropped)
 
 
-def mycode():
+                k = cv2.waitKey(1)
+                if k == 0xFF & ord("q"):
+                    break
+
+
+def mycode(style):
+
     #TODO make a folder and check for it - read components
 
     if os.path.exists(os.path.join(os.getcwd(),'on_the_board')):
         pass
     else:
         os.mkdir(os.path.join(os.getcwd(),'on_the_board'))
-
-    # tf code
-
-    x = ['''<!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>New project</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-        <script src="main.js"></script>
-    </head>
-    <body>
-    
-    '''
-    ,
-    ''' 
-    </body>
-    </html>''']
-
-    html_tool = {'searchbar':'''<nav>
-                        <div class="nav-wrapper z-depth-4">
-                          <form>
-                            <div class="input-field z-depth-4">
-                              <input id="search" type="search" required>
-                              <label class="label-icon right" for="search"><i class="material-icons">maps</i></label>
-                              <i class="material-icons">close</i>
-                            </div>
-                          </form>
-                        </div>
-                    </nav>''',
-                 'button':'<button class="btn btn-large black">Read More</button>',
-                 'random_para':'<h4 class="truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit porro eius esse, qui sequi tempore impedit ea, similique nulla neque ad velit minus deleniti quibusdam laborum voluptatem explicabo! Blanditiis, expedita.</h4>',
-                 'list':'''<ul class="collection">
-              <li class="collection-item">Item 1</li>
-              <li class="collection-item">Item 2</li>
-              <li class="collection-item">Item 3</li>
-              <li class="collection-item">Item 4</li>
-            </ul>''',
-                 'header':'''<h2 style="font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">Header</h2>''',
-                 'input_text':'''<div class="input-field">
-               <input type="text" id="label">
-               <label class="active" for="name">here</label>
-             </div>''',
-                 'input_textarea':'''<div class="input-field">
-               <textarea type="textarea" id="textarea" class="materialize-textarea"></textarea>
-               <label class="active" for="textarea">Textarea</label>
-             </div>''',
-                 'grid_div2':'''<div class="row">
-           <div class="col s6"><div class="card-panel grey">boop</div></div>
-         </div>''',
-                 'navig':'''<nav>
-            <div class="nav-wrapper blue">
-              <h3 class="center"> heading </h3>
-              </div>
-          </nav>'''
-                 }
 
 
 
@@ -186,6 +271,5 @@ def targeter(image):
         bbox = plt.connect('key_press_event', toggle_selector)
         key = plt.connect('key_press_event', onkeypress)
         plt.show()
-
 
 yolo()
